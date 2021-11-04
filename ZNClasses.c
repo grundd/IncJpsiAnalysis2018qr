49cefb2883f09b61f01f3bc3be1c44c6447b692f
grundd <david.grund@cern.ch>
2021-11-04 08:47:54 -0500

    Update no. 1 04-11-2021

--- ZNClasses.c
+++ ZNClasses.c
@@ -1,27 +1,54 @@
 // ZNClasses.c
 // David Grund, Nov 3, 2021
 
+// cpp headers
+#include <fstream> // print output to txt file
+#include <iomanip> // std::setprecision()
 // root headers
 #include "TFile.h"
 #include "TTree.h"
 #include "TMath.h"
+#include "TString.h"
 // my headers
 #include "AnalysisManager.h"
 
 void PrepareData();
-void CalculateClasses();
+void CalculateClasses(Int_t iMassCut);
 Bool_t EventPassedZN(Int_t iMassCut, Int_t iPtCut, Int_t iPtBin);
+Double_t CalculateErrorBayes(Double_t k, Double_t n);
+
+Double_t nEvTotal[nPtBins] = { 0 };        // total number of events per bin
+Double_t nEvFired[nPtBins] = { 0 };        // number of events per bin with at least one fired ZN
+Double_t nEvFired_ZNA[nPtBins] = { 0 };    // number of events per bin with fired ZNA
+Double_t nEvFired_ZNC[nPtBins] = { 0 };    // number of events per bin with fired ZNC
+Double_t nEvFired_OnlyZNA[nPtBins] = { 0 };// number of events per bin with fired ZNA only
+Double_t nEvFired_OnlyZNC[nPtBins] = { 0 };// number of events per bin with fired ZNC only
+Double_t nEvFired_both[nPtBins] = { 0 };   // number of events per bin with fired both ZN
+Double_t nEvFired_none[nPtBins] = { 0 };   // number of events per bin with no fired ZN
+Double_t nSums[8] = { 0 };
+
+// Percentages (relative to the total number of events in a bin)
+Double_t PercFiredBoth_val[nPtBins] = { 0 }; 
+Double_t PercFiredNone_val[nPtBins] = { 0 }; 
+Double_t PercFiredZNAOnly_val[nPtBins] = { 0 }; 
+Double_t PercFiredZNCOnly_val[nPtBins] = { 0 }; 
+Double_t PercFiredBoth_err[nPtBins] = { 0 }; 
+Double_t PercFiredNone_err[nPtBins] = { 0 }; 
+Double_t PercFiredZNAOnly_err[nPtBins] = { 0 }; 
+Double_t PercFiredZNCOnly_err[nPtBins] = { 0 }; 
 
 void ZNClasses(){
 
     //PrepareData();
 
-    CalculateClasses();
+    CalculateClasses(0); // Around J/psi peak: 3.0 < m < 3.2 GeV
+
+    CalculateClasses(1); // Side-bands: 2.5 < m < 2.9 GeV && 3.3 < m < 4.0 GeV
 
     return;
 }
 
-void CalculateClasses(){
+void CalculateClasses(Int_t iMassCut){
 
     TFile *fFileIn = TFile::Open("Trees/ZNClasses/ZNClasses.root", "read");
     if(fFileIn) Printf("Input data loaded.");
@@ -45,14 +72,121 @@ void CalculateClasses(){
         fTreeIn->GetEntry(iEntry);
 
         for(Int_t iBin = 0; iBin < nPtBins; iBin++){
-            if(EventPassedZN(0, 1, iBin+1));
+            if(EventPassedZN(iMassCut, 1, iBin+1)){
+                Bool_t ZN_fired = kFALSE;
+                Bool_t ZNA_fired = kFALSE;
+                Bool_t ZNC_fired = kFALSE;
+                for(Int_t i = 0; i < 4; i++){
+                    // Check ZN times: at least one time has to fall within pm 2 ns
+                    if(TMath::Abs(fZNA_time[i]) < 2 || TMath::Abs(fZNC_time[i]) < 2) ZN_fired = kTRUE;
+                    if(TMath::Abs(fZNA_time[i]) < 2) ZNA_fired = kTRUE;
+                    if(TMath::Abs(fZNC_time[i]) < 2) ZNC_fired = kTRUE;
+                }
+                nEvTotal[iBin]++;
+                if(ZN_fired) nEvFired[iBin]++;
+                if(ZNA_fired && ZNC_fired) nEvFired_both[iBin]++;
+                if(ZNA_fired) nEvFired_ZNA[iBin]++;
+                if(ZNC_fired) nEvFired_ZNC[iBin]++;
+                if(ZNA_fired && !ZNC_fired) nEvFired_OnlyZNA[iBin]++;
+                if(ZNC_fired && !ZNA_fired) nEvFired_OnlyZNC[iBin]++;
+                if(!ZNA_fired && !ZNC_fired) nEvFired_none[iBin]++;
+            }
         }
-                
+
         if((iEntry+1) % 500 == 0){
             nEntriesAnalysed += 500;
             Printf("%i entries analysed.", nEntriesAnalysed);
         }
     }
+
+    // Calculate percentages
+    for(Int_t iBin = 0; iBin < nPtBins; iBin++){
+        PercFiredBoth_val[iBin] = nEvFired_both[iBin] / nEvTotal[iBin] * 100;
+        PercFiredBoth_err[iBin] = CalculateErrorBayes(nEvFired_both[iBin], nEvTotal[iBin]) * 100;
+        PercFiredNone_val[iBin] = nEvFired_none[iBin] / nEvTotal[iBin] * 100;
+        PercFiredNone_err[iBin] = CalculateErrorBayes(nEvFired_none[iBin], nEvTotal[iBin]) * 100;
+        PercFiredZNAOnly_val[iBin] = nEvFired_OnlyZNA[iBin] / nEvTotal[iBin] * 100;
+        PercFiredZNAOnly_err[iBin] = CalculateErrorBayes(nEvFired_OnlyZNA[iBin], nEvTotal[iBin]) * 100;
+        PercFiredZNCOnly_val[iBin] = nEvFired_OnlyZNC[iBin] / nEvTotal[iBin] * 100;
+        PercFiredZNCOnly_err[iBin] = CalculateErrorBayes(nEvFired_OnlyZNC[iBin], nEvTotal[iBin]) * 100;
+    }
+
+    // Print the results to console
+    // Numbers
+    Printf("Numbers of events:");
+    Printf("Bin \tTotal \tFired \tZNA \tZNC \tOnlyZNA\tOnlyZNC\tBoth \tNone");
+    for(Int_t iBin = 0; iBin < nPtBins; iBin++){
+        nSums[0] += nEvTotal[iBin];
+        nSums[1] += nEvFired[iBin];
+        nSums[2] += nEvFired_ZNA[iBin];
+        nSums[3] += nEvFired_ZNC[iBin];
+        nSums[4] += nEvFired_OnlyZNA[iBin];
+        nSums[5] += nEvFired_OnlyZNC[iBin];
+        nSums[6] += nEvFired_both[iBin];
+        nSums[7] += nEvFired_none[iBin];
+        Printf("%i \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f", 
+            iBin+1, 
+            nEvTotal[iBin], 
+            nEvFired[iBin], 
+            nEvFired_ZNA[iBin], 
+            nEvFired_ZNC[iBin], 
+            nEvFired_OnlyZNA[iBin], 
+            nEvFired_OnlyZNC[iBin], 
+            nEvFired_both[iBin],
+            nEvFired_none[iBin]);
+    }
+    Printf("sum \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f", nSums[0], nSums[1], nSums[2], nSums[3], nSums[4], nSums[5], nSums[6], nSums[7]);
+    // Percentages
+    Printf("Percentages:");
+    Printf("Bin \tBoth[%%]\terr \tZNA[%%]\terr \tZNC[%%]\terr \tNone[%%]\terr");
+    for(Int_t iBin = 0; iBin < nPtBins; iBin++){
+        Printf("%i \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f", 
+            iBin+1,
+            PercFiredBoth_val[iBin],
+            PercFiredBoth_err[iBin],
+            PercFiredZNAOnly_val[iBin],
+            PercFiredZNAOnly_err[iBin],
+            PercFiredZNCOnly_val[iBin],
+            PercFiredZNCOnly_err[iBin],
+            PercFiredNone_val[iBin],
+            PercFiredNone_err[iBin]);
+    }
+
+    // Print numbers to txt files
+    TString FilePath = Form("Results/ZNClasses/%ibins_MassInterval%i_output.txt", nPtBins, iMassCut);
+    ofstream outfile(FilePath.Data());
+    outfile << Form("Numbers of events:\n");
+    outfile << Form("Bin \tTotal \tFired \tZNA \tZNC \tOnlyZNA\tOnlyZNC\tBoth \tNone\n");
+    for(Int_t iBin = 0; iBin < nPtBins; iBin++){
+        outfile << Form("%i \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \n", 
+            iBin+1, 
+            nEvTotal[iBin], 
+            nEvFired[iBin], 
+            nEvFired_ZNA[iBin], 
+            nEvFired_ZNC[iBin], 
+            nEvFired_OnlyZNA[iBin], 
+            nEvFired_OnlyZNC[iBin], 
+            nEvFired_both[iBin],
+            nEvFired_none[iBin]);
+    }
+    outfile << Form("sum \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \t%.0f \n", nSums[0], nSums[1], nSums[2], nSums[3], nSums[4], nSums[5], nSums[6], nSums[7]);
+    outfile << Form("\n\n");
+    outfile << Form("Percentages:\n");
+    outfile << Form("Bin \tBoth[%%]\terr \tZNA[%%]\terr \tZNC[%%]\terr \tNone[%%]\terr \n");
+    for(Int_t iBin = 0; iBin < nPtBins; iBin++){
+        outfile << Form("%i \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \t%.1f \n", 
+            iBin+1,
+            PercFiredBoth_val[iBin],
+            PercFiredBoth_err[iBin],
+            PercFiredZNAOnly_val[iBin],
+            PercFiredZNAOnly_err[iBin],
+            PercFiredZNCOnly_val[iBin],
+            PercFiredZNCOnly_err[iBin],
+            PercFiredNone_val[iBin],
+            PercFiredNone_err[iBin]);
+    }
+    outfile.close();
+    Printf("Results printed to %s.", FilePath.Data()); 
 
     return;
 }
@@ -113,7 +247,7 @@ Bool_t EventPassedZN(Int_t iMassCut, Int_t iPtCut, Int_t iPtBin){
 
     // Transverse momentu cut
     Bool_t bPtCut = kFALSE;
-    switch(iPtBin){
+    switch(iPtCut){
         case 0:
             if(fPt > 0.2 && fPt < 1.0) bPtCut = kTRUE;
             break;
@@ -123,13 +257,14 @@ Bool_t EventPassedZN(Int_t iMassCut, Int_t iPtCut, Int_t iPtBin){
     }
     if(!bPtCut) return kFALSE;
 
-    // 10) Check ZN times: at least one time has to fall within pm 2 ns
-    Bool_t ZN_times = kFALSE;
-    for(Int_t i = 0; i < 4; i++){
-        if(TMath::Abs(fZNA_time[i]) < 2 || TMath::Abs(fZNC_time[i]) < 2) ZN_times = kTRUE;
-    }
-    if(!ZN_times) return kFALSE;
-
     // Event passed all the selections =>
     return kTRUE;
+}
+
+Double_t CalculateErrorBayes(Double_t k, Double_t n){ // k = nominator, n = denominator
+
+    Double_t var = (k + 1) * (k + 2) / (n + 2) / (n + 3) - (k + 1) * (k + 1) / (n + 2) / (n + 2);
+    Double_t err = TMath::Sqrt(var);
+
+    return err;
 }
